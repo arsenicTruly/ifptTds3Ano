0908c8eb4777611331abbcacbbd088b421cf0b49
AnaBatista <pobre6877@gmail.com>
2026-02-25 09:26:51 -0300

    Update exercicio.php

--- LPW/aula2/lista3/exercicio.php
+++ LPW/aula2/lista3/exercicio.php
@@ -1,55 +1,6 @@
 <?php
 
-class Presidente {
-    private int $numero;
-    private string $nome;
-    private int $inicio;
-    private int $fim;
-
-    public function __construct($num, $nome, $ini, $fim){
-        $this->numero = $num;
-        $this->nome = $nome;
-        $this->inicio = $ini;
-        $this->fim = $fim;
-    }
-
-    //Getters e Setters
-        // Getter e Setter do número
-        public function getNumero(): int {
-            return $this->numero;
-        }
-
-        public function setNumero(int $numero): void {
-            $this->numero = $numero;
-        }
-
-        // Getter e Setter do nome
-        public function getNome(): string {
-            return $this->nome;
-        }
-
-        public function setNome(string $nome): void {
-            $this->nome = $nome;
-        }
-
-        // Getter e Setter do início do mandato
-        public function getInicio(): int {
-            return $this->inicio;
-        }
-
-        public function setInicio(int $inicio): void {
-            $this->inicio = $inicio;
-        }
-
-        // Getter e Setter do fim do mandato
-        public function getFim(): int {
-            return $this->fim;
-        }
-
-        public function setFim(int $fim): void {
-            $this->fim = $fim;
-        }
-}
+require_once("modelo/Presidente.php");
 
 $lista = array();
 
@@ -75,17 +26,7 @@ echo "<table>";
     echo "</tr>";
     foreach($lista as $linha => $presidente){
         echo "<tr>";
-
-            for ($i = 0; $i < 4; $i++) {
-                    echo "<td>";
-                    switch ($i) {
-                        case 0: echo $presidente->getNumero(); break;
-                        case 1: echo $presidente->getNome(); break;
-                        case 2: echo $presidente->getInicio(); break;
-                        case 3: echo $presidente->getFim(); break;
-                    }
-                    echo"</td>";
-            }
+        echo $presidente->getLinhaTabela;
         echo "</tr>";
     }
 echo "</table>"
