9ec25aa3fbe4f8daa314cf44b1ca3019b9a3c746
AnaBatista <pobre6877@gmail.com>
2026-02-25 09:42:26 -0300

    Update exercicio.php

--- LPW/aula2/lista2/exercicio.php
+++ LPW/aula2/lista2/exercicio.php
@@ -1,29 +1,33 @@
 <?php 
-
 $cabecalho = array("Ordem", "Pais", "Ouro", "Prata", "Bronze", "Total");
-$USA = array("1", "USA - Estados Unidos", "46", "37", "38", "121", "bandeiraUSA.jpg");
-$GBR = array("2", "GBR - Gra-Bretanha", "27", "23", "12", "67", "bandeiraGBR.jpg");
-$CHN = array("3", "CHN - China", "26", "18", "26", "70", "bandeiraCHN.jpg");
-$RUS = array("4", "RUS - Russia", "19", "17", "20", "56", "bandeiraRUS.jpg");
-$GER = array("5", "GER - Alemanha", "17", "10", "15", "42", "bandeiraGER.jpg");
+$USA = array("1", "USA - Estados Unidos", "46", "37", "38", "121", "bandeiraUSA.jpg", "https://pt.wikipedia.org/wiki/Estados_Unidos");
+$GBR = array("2", "GBR - Gra-Bretanha", "27", "23", "12", "67", "bandeiraGBR.jpg", "https://pt.wikipedia.org/wiki/Grã-Bretanha");
+$CHN = array("3", "CHN - China", "26", "18", "26", "70", "bandeiraCHN.jpg", "https://pt.wikipedia.org/wiki/China");
+$RUS = array("4", "RUS - Russia", "19", "17", "20", "56", "bandeiraRUS.jpg", "https://pt.wikipedia.org/wiki/Russia");
+$GER = array("5", "GER - Alemanha", "17", "10", "15", "42", "bandeiraGER.jpg", "https://pt.wikipedia.org/wiki/Alemanha");
 
 $tabela = array();
 array_push($tabela, $cabecalho, $USA, $GBR, $CHN, $RUS, $GER);
 ?>
-<table>
+
+<table style="border='1'">
     <?php
         foreach ($tabela as $linha => $row) {
             echo "<tr>";
-            for ($i = 0; $i < count($row); $i++) {
+            for ($i = 0; $i < count($row) - 2; $i++) {
                 if ($linha == 0) {
                     echo "<th style='background-color: grey'>{$row[$i]}</th>";
                 } else {
                     echo "<td>";
-                    if ($i == 1) {
-                        echo "<img src='{$row[6]}' alt='Bandeira' width='25'> ";
-                    }
-                    echo $row[$i];
-                    echo"</td>";
+                        if ($i == 1) {
+                            echo "<img src='{$row[6]}' alt='Bandeira' width='25'> ";
+                            echo "<a href='{$row[7]}'>";
+                            echo $row[$i];
+                            echo "</a>";
+                        } else {
+                            echo $row[$i];
+                        }
+                    echo "</td>";
                 }
             }
             echo "</tr>";
