ece22a65be520e33724b66e8a0ac71014cec776b
AnaBatista <pobre6877@gmail.com>
2026-02-25 09:30:18 -0300

    Update exercicio.php

--- LPW/aula2/lista2/exercicio.php
+++ LPW/aula2/lista2/exercicio.php
@@ -1,11 +1,11 @@
 <?php 
 
 $cabecalho = array("Ordem", "Pais", "Ouro", "Prata", "Bronze", "Total");
-$USA = array("1", "USA - Estados Unidos", "46", "37", "38", "121");
-$GBR = array("2", "GBR - Gra-Bretanha", "27", "23", "12", "67");
-$CHN = array("3", "CHN - China", "26", "18", "26", "70");
-$RUS = array("4", "RUS - Russia", "19", "17", "20", "56");
-$GER = array("5", "GER - Alemanha", "17", "10", "15", "42");
+$USA = array("1", "USA - Estados Unidos", "46", "37", "38", "121", "bandeiraUSA.jpg");
+$GBR = array("2", "GBR - Gra-Bretanha", "27", "23", "12", "67", "bandeiraGBR.jpg");
+$CHN = array("3", "CHN - China", "26", "18", "26", "70", "bandeiraCHN.jpg");
+$RUS = array("4", "RUS - Russia", "19", "17", "20", "56", "bandeiraRUS.jpg");
+$GER = array("5", "GER - Alemanha", "17", "10", "15", "42", "bandeiraGER.jpg");
 
 $tabela = array();
 array_push($tabela, $cabecalho, $USA, $GBR, $CHN, $RUS, $GER);
@@ -18,7 +18,12 @@ array_push($tabela, $cabecalho, $USA, $GBR, $CHN, $RUS, $GER);
                 if ($linha == 0) {
                     echo "<th style='background-color: grey'>{$row[$i]}</th>";
                 } else {
-                    echo "<td>{$row[$i]}</td>";
+                    echo "<td>";
+                    if ($i == 1) {
+                        echo "<img src='{$row[6]}' alt='Bandeira' width='25'> ";
+                    }
+                    echo $row[$i];
+                    echo"</td>";
                 }
             }
             echo "</tr>";
